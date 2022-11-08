231d1013b11c0c90c73b2a0d3f5f6466dd22f4cd
dgouron <damien.gouron@gmail.com>
2022-11-08 17:30:52 +0100

    3 methods for find the average

--- average/average.js
+++ average/average.js
@@ -5,4 +5,22 @@ const average = (array) => {
   return reducer(array, 0) / array.length;
 };
 
-module.exports = average;
+const averageWithLoop = (array) => {
+  if (array.length === 0) return 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
+};
+
+const averageWithReduce = (array) => {
+  if (array.length === 0) return 0;
+  return array.reduce((sum, current) => sum + current, 0) / array.length;
+};
+
+module.exports = {
+  average,
+  averageWithLoop,
+  averageWithReduce,
+};
--- average/average.test.js
+++ average/average.test.js
@@ -1,4 +1,4 @@
-const average = require("./average");
+const { average, averageWithLoop, averageWithReduce } = require("./average");
 
 test("average", () => {
   expect(average([1, 3, 44])).toBe(16);
@@ -6,3 +6,19 @@ test("average", () => {
   expect(average([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(5.5);
   expect(average([])).toBe(0);
 });
+
+test("averageWithLoop", () => {
+  expect(averageWithLoop([1, 3, 44])).toBe(16);
+  expect(averageWithLoop([1, 2, 3, 4, 5])).toBe(3);
+  expect(averageWithLoop([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(5.5);
+  expect(averageWithLoop([])).toBe(0);
+});
+
+test("averageWithReduce", () => {
+  expect(averageWithReduce([1, 3, 44])).toBe(16);
+  expect(averageWithReduce([1, 2, 3, 4, 5])).toBe(3);
+  expect(averageWithReduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(5.5);
+  expect(averageWithReduce([])).toBe(0);
+});
+
+// Path: average\average.js
