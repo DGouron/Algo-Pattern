123cfdb8f24cbb31482230710d9bb9b61884a764
dgouron <damien.gouron@gmail.com>
2023-03-17 18:20:06 +0100

    Feat : debouncing
    - Add jsdom and jest config
    - Add debounce.js with exemple
    - Add debounce test using jsdom

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    html: '<html lang="zh-cmn-Hant"></html>',
+    url: 'https://jestjs.io/',
+    userAgent: 'Agent/007',
+  },
+};
+
+module.exports = config;
